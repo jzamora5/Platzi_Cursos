259664ee098b405140e8a40c75701382803095b3
jzamora5 <jzamora_5@yahoo.com>
2021-01-18 10:15:23 -0500

    HistoryApifallback

---
+++ ReactRouterRedux/PlatziVideo/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
+
+const Header = () => (
+  <header className="header">
+    <img className="header__img" src={logo} alt="Platzi Video" />
+    <div className="header__menu">
+      <div className="header__menu--profile">
+        <img src={userIcon} alt="" />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href="/">Cuenta</a></li>
+        <li><a href="/">Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ ReactRouterRedux/PlatziVideo/src/containers/Home.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
+import useInitialState from "../hooks/useInitialState";
+import "../assets/styles/App.scss";
+
+const API = "http://localhost:3000/initalState";
+
+const Home = () => {
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="App">
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            {initialState.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
