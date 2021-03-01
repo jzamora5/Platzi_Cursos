35b312b43ae01e9559ac092e360196ac2ab434b2
jzamora5 <jzamora_5@yahoo.com>
2021-03-01 18:48:17 -0500

    Change spider method to generator in order to save to file

--- Scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ Scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -12,27 +12,17 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n\n')
 
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo: {title}')
-        print('\n\n')
 
         quotes = response.xpath(
             '//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Citas: ')
-        for quote in quotes:
-            print(f'- {quote}')
-        print('\n\n')
 
         top_ten_tags = response.xpath(
             '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-        print('Top ten tags: ')
-        for tag in top_ten_tags:
-            print(f'- {tag}')
-        print('\n\n')
 
-        # print(response.status, response.headers)
-        print('\n\n')
-        print('*' * 10)
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
