fa67726fe89caac8d85ac22394669d84264fff71
jzamora5 <jzamora_5@yahoo.com>
2021-03-04 18:47:36 -0500

    pass arguments to spider

--- Scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ Scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -39,12 +39,18 @@ class QuotesSpider(scrapy.Spider):
         quotes = response.xpath(
             '//span[@class="text" and @itemprop="text"]/text()').getall()
 
-        top_ten_tags = response.xpath(
+        top_tags = response.xpath(
             '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+
+        top = getattr(self, 'top', None)
+
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
 
         yield {
             'title': title,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
         next_page_button_link = response.xpath(
