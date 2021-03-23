aa264c025db1719a11f3d997d4f6c72f72b22790
jzamora5 <jzamora_5@yahoo.com>
2021-03-22 19:59:34 -0500

    Spider Base for CIA links

---
+++ Scrapy/platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
@@ -0,0 +1,27 @@
+import scrapy
+
+# XPath
+
+# links = //a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href
+
+
+class SpiderCIA(scrapy.spider):
+    name = "cia"
+    start_urls = [
+        "https://www.cia.gov/readingroom/historical-collections"
+    ]
+    custom_settings = {
+        'FEED_URI': 'cia.json',
+        'FEED_FORMAT': 'json',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+
+    def parse(self, response):
+        links_desclassified = response.xpath(
+            '//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
+
+        for link in links_desclassified:
+            yield response.follow(link, callback=self.parse_link)
+
+    def parse_link(self, response):
+        pass
