43ad0b455f34b3e2785cf1869e7fdf7e2c98609e
jzamora5 <jzamora_5@yahoo.com>
2021-03-23 20:00:40 -0500

    Complete spider

--- Scrapy/platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
+++ Scrapy/platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
@@ -3,9 +3,11 @@ import scrapy
 # XPath
 
 # links = //a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href
+# title = //h1[@class="documentFirstHeading"]/text()
+# paragraph = //div[@class="field-item even"]//p[not(@class)]/text()
 
 
-class SpiderCIA(scrapy.spider):
+class SpiderCIA(scrapy.Spider):
     name = "cia"
     start_urls = [
         "https://www.cia.gov/readingroom/historical-collections"
@@ -21,7 +23,17 @@ class SpiderCIA(scrapy.spider):
             '//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
 
         for link in links_desclassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath(
+            '//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath(
+            '//div[@class="field-item even"]//p[not(@class)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
