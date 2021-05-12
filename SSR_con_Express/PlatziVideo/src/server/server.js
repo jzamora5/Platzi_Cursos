9c63d4c0c47e0e478960c26adc9d04fada83fbe8
jzamora5 <jzamora_5@yahoo.com>
2021-05-12 18:18:38 -0500

    Add Helmet to Express

--- SSR_con_Express/PlatziVideo/src/server/server.js
+++ SSR_con_Express/PlatziVideo/src/server/server.js
@@ -1,9 +1,10 @@
 /* eslint-disable global-require */
+import React from 'react';
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import helmet from 'helmet';
 
-import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -30,6 +31,11 @@ if (ENV === 'development') {
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
 const setResponse = (html, preloadedState) => {
@@ -67,6 +73,7 @@ const renderApp = (req, res) => {
     </Provider>,
   );
 
+  res.removeHeader('X-Powered-By');
   res.send(setResponse(html, preloadedState));
 };
 
