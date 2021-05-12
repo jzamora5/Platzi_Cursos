311c73db769033797d33118930da071ab5193920
jzamora5 <jzamora_5@yahoo.com>
2021-05-12 17:51:35 -0500

    Try catch for request

--- Redux/blog_platzi/src/actions/usuariosActions.js
+++ Redux/blog_platzi/src/actions/usuariosActions.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import { TRAER_TODOS } from '../types/usuariosTypes';
 
 export const traerTodos = () => async (dispatch) => {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users',
-  );
-
-  dispatch({
-    type: TRAER_TODOS,
-    payload: response.data,
-  });
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    dispatch({
+      type: TRAER_TODOS,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log('Error: ', error.message);
+  }
 };
