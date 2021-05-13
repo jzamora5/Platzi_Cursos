7799ba2cf9dd6a47c4ca68815963b8334461d36e
jzamora5 <jzamora_5@yahoo.com>
2021-05-12 19:43:57 -0500

    Optimize build with compression and terser web pack plugins

--- SSR_con_Express/PlatziVideo/webpack.config.js
+++ SSR_con_Express/PlatziVideo/webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 require('dotenv').config();
 
-const isDev = (process.env.ENV === 'development');
+const isDev = process.env.ENV === 'development';
 const entry = ['./src/frontend/index.js'];
 
 if (isDev) {
-  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true');
+  entry.push(
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
+  );
 }
 
 module.exports = {
@@ -22,22 +26,18 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(s*)css$/,
@@ -46,28 +46,33 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
           },
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|gif|jpg)$/,
         use: [
           {
-            'loader': 'file-loader',
+            loader: 'file-loader',
             options: {
-              name: 'assets/[hash].[ext]'
-            }
-          }
-        ]
-      }
-    ]
+              name: 'assets/[hash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
   },
   plugins: [
-    isDev ? new webpack.HotModuleReplacementPlugin() :
-      () => { },
+    isDev ? new webpack.HotMosduleReplacementPlugin() : () => {},
+    isDev ?
+      () => {} :
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$/,
+        filename: '[path].gz',
+      }),
     new MiniCssExtractPlugin({
       filename: 'assets/app.css',
     }),
