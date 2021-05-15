8c2e56f1b648bf6f4ff053306671aa12ef69d4b5
jzamora5 <jzamora_5@yahoo.com>
2021-05-14 20:25:51 -0500

    Divide into smaller components

--- Redux/blog_platzi/src/components/Usuarios/index.js
+++ Redux/blog_platzi/src/components/Usuarios/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
+import Tabla from './Tabla';
 
 class Usuarios extends Component {
   componentDidMount() {
@@ -15,35 +17,28 @@ class Usuarios extends Component {
   }
 
   ponerContenido = () => {
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{this.ponerFilas()}</tbody>
-      </table>
-    );
-  };
+    if (this.props.cargando) {
+      return <Spinner />;
+    }
 
-  ponerFilas = () =>
-    this.props.usuarios.map((usuario) => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
+    if (this.props.error) {
+      return <Fatal mensaje={this.props.error} />;
+    }
+
+    return <Tabla />;
+  };
 
   render() {
     if (this.props.cargando) {
       return <Spinner />;
     }
 
-    return <div>{this.ponerContenido()}</div>;
+    return (
+      <div>
+        <h1>Usuarios</h1>
+        {this.ponerContenido()}
+      </div>
+    );
   }
 }
 
