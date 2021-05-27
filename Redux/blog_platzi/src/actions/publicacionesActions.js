792327ff39fc2a73650757e5326a15ef80d5559c
jzamora5 <jzamora_5@yahoo.com>
2021-05-26 22:50:58 -0500

    Immutability

--- Redux/blog_platzi/src/actions/publicacionesActions.js
+++ Redux/blog_platzi/src/actions/publicacionesActions.js
@@ -4,6 +4,9 @@ import {
   CARGANDO,
   ERROR,
 } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -17,6 +20,18 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
   );
 
   const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key,
+  };
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados,
+  });
 
   dispatch({
     type: TRAER_POR_USUARIO,
