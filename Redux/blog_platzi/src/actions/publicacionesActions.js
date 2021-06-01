7e3ce4e18f2699e4e0ea877ff7dcdf0776d6cdf8
jzamora5 <jzamora_5@yahoo.com>
2021-05-31 20:02:25 -0500

    Add base action for publication comments

--- Redux/blog_platzi/src/actions/publicacionesActions.js
+++ Redux/blog_platzi/src/actions/publicacionesActions.js
@@ -9,32 +9,54 @@ import * as usuariosTypes from '../types/usuariosTypes';
 const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
+  dispatch({
+    type: CARGANDO,
+  });
+
   const { usuarios } = getState().usuariosReducer;
   const { publicaciones } = getState().publicacionesReducer;
   const usuario_id = usuarios[key].id;
 
   console.log(publicaciones);
 
-  const respuesta = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
-  );
+  try {
+    const respuesta = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
+    );
 
-  const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+    const nuevas = respuesta.data.map((publicacion) => ({
+      ...publicacion,
+      comentarios: [],
+      abierto: false,
+    }));
 
-  const publicaciones_key = publicaciones_actualizadas.length - 1;
-  const usuarios_actualizados = [...usuarios];
-  usuarios_actualizados[key] = {
-    ...usuarios[key],
-    publicaciones_key,
-  };
+    const publicaciones_actualizadas = [...publicaciones, nuevas];
 
-  dispatch({
-    type: USUARIOS_TRAER_TODOS,
-    payload: usuarios_actualizados,
-  });
+    dispatch({
+      type: TRAER_POR_USUARIO,
+      payload: publicaciones_actualizadas,
+    });
 
-  dispatch({
-    type: TRAER_POR_USUARIO,
-    payload: publicaciones_actualizadas,
-  });
+    const publicaciones_key = publicaciones_actualizadas.length - 1;
+    const usuarios_actualizados = [...usuarios];
+    usuarios_actualizados[key] = {
+      ...usuarios[key],
+      publicaciones_key,
+    };
+
+    dispatch({
+      type: USUARIOS_TRAER_TODOS,
+      payload: usuarios_actualizados,
+    });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: ERROR,
+      payload: 'Publicaciones no disponibles',
+    });
+  }
+};
+
+export const abrirCerrar = (pub_key, com_key) => (dispatch) => {
+  console.log(pub_key, com_key);
 };
--- Redux/blog_platzi/src/components/Publicaciones/index.js
+++ Redux/blog_platzi/src/components/Publicaciones/index.js
@@ -7,7 +7,8 @@ import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionesActions from '../../actions/publicacionesActions';
 
 const { traerTodos: usuariosTraerTodos } = usuariosActions;
-const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
+const { traerPorUsuario: publicacionesTraerPorUsuario, abrirCerrar } =
+  publicacionesActions;
 
 class Publicaciones extends Component {
   async componentDidMount() {
@@ -53,12 +54,60 @@ class Publicaciones extends Component {
     return <h1>Publicaciones de {nombre}</h1>;
   };
 
+  ponerPublicaciones = () => {
+    const {
+      usuariosReducer,
+      usuariosReducer: { usuarios },
+      publicacionesReducer,
+      publicacionesReducer: { publicaciones },
+      match: {
+        params: { key },
+      },
+    } = this.props;
+
+    if (!usuarios.length) return;
+    if (usuariosReducer.error) return;
+
+    if (publicacionesReducer.cargando) {
+      return <Spinner />;
+    }
+
+    if (publicacionesReducer.error) {
+      return <Fatal mensaje={publicacionesReducer.error} />;
+    }
+
+    if (!publicaciones.length) return;
+
+    if (!('publicaciones_key' in usuarios[key])) return;
+
+    const { publicaciones_key } = usuarios[key];
+
+    return this.mostrarInfo(
+      publicaciones[publicaciones_key],
+      publicaciones_key
+    );
+  };
+
+  mostrarInfo = (publicaciones, pub_key) =>
+    publicaciones.map((publicacion, com_key) => {
+      return (
+        <div
+          className="pub_titulo"
+          key={publicacion.id}
+          onClick={() => this.props.abrirCerrar(pub_key, com_key)}
+        >
+          <h2>{publicacion.title}</h2>
+          <h3>{publicacion.body}</h3>
+        </div>
+      );
+    });
+
   render() {
     console.log(this.props);
     return (
       <div>
-        {this.props.match.params.key}
         {this.ponerUsuario()}
+        {this.ponerPublicaciones()}
       </div>
     );
   }
@@ -71,6 +120,7 @@ const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => {
 const mapDispatchToProps = {
   usuariosTraerTodos,
   publicacionesTraerPorUsuario,
+  abrirCerrar,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
