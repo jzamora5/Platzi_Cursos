a4944ce191a993f4a849d96a859f381bca843094
jzamora5 <jzamora_5@yahoo.com>
2021-06-10 16:14:58 -0500

    Add routes for userMovies

---
+++ Autenticacion_con_PassportJS/movies-api/routes/userMovies.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const UserMovieService = require('../services/userMovies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
+const { movieIdSchema } = require('../utils/schemas/movies');
+const { userIdSchema } = require('../utils/schemas/users');
+const { createUserMovieSchema } = require('../utils/schemas/userMovies');
+
+function userMoviesApi(app) {
+  const router = express.router();
+
+  app.use('/api/user-movies', router);
+
+  const userMoviesService = new UserMovieService();
+
+  router.get(
+    '/',
+    validationHandler({ userId: userIdSchema }, 'query'),
+    async function(req, res, next) {
+      const { userId } = req.query;
+      try {
+        const userMovies = await userMoviesService.getUserMovies({ userId });
+
+        res.status(200).json({
+          data: userMovies,
+          message: 'user movies listed',
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+  router.post('/', validationHandler(createUserMovieSchema), async function(
+    req,
+    res,
+    next
+  ) {
+    const { body: userMovie } = req;
+
+    try {
+      const createUserMovieId = await userMoviesService.createUserMovie({
+        userMovie,
+      });
+
+      res.status(201).json({
+        data: createUserMovieId,
+        message: 'user movie created',
+      });
+    } catch (err) {
+      next(err);
+    }
+
+    res.status(201).json;
+  });
+
+  router.delete(
+    '/:userMovieId',
+    validationHandler({ userMovieId: movieIdSchema }, 'params'),
+    async function(req, res, next) {
+      const { userMovieId } = req.params;
+
+      try {
+        const deletedUserMovieId = await UserMovieService.deletedUserMovieId({
+          userMovieId,
+        });
+        res.status(200).json({
+          data: deletedUserMovieId,
+          message: 'user movie deleted',
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+}
+
+module.exports = userMoviesApi;
