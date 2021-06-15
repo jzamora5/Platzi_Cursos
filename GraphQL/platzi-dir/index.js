4c4aa55a7c7fe365dc03539a58fef713521fa761
jzamora5 <jzamora_5@yahoo.com>
2021-06-15 11:48:29 -0500

    Add query to a specific resource

--- GraphQL/platzi-dir/index.js
+++ GraphQL/platzi-dir/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs');
@@ -11,10 +11,15 @@ const app = express();
 const port = 3000;
 
 // definiendo el esquema
-
-const schema = buildSchema(
-  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 );
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 // Ejecutar el query
 
