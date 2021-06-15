f314010ed2bf4659dcb84f291ecc2996bda6a38f
jzamora5 <jzamora_5@yahoo.com>
2021-06-15 11:31:08 -0500

    Organize GraphQL Files and add Custom Type

--- GraphQL/platzi-dir/index.js
+++ GraphQL/platzi-dir/index.js
@@ -1,27 +1,20 @@
 'use strict';
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000;
 
 // definiendo el esquema
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
-
-// Configurar los resolvers
-
-const resolvers = {
-  hello: () => {
-    return 'Hola Mundo';
-  },
-};
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+);
 
 // Ejecutar el query
 
