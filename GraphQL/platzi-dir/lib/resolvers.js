dc75a06c663c1a3249a407bf85445e2d73090b03
jzamora5 <jzamora_5@yahoo.com>
2021-06-16 11:16:38 -0500

    Add mutation to insert data

--- GraphQL/platzi-dir/lib/resolvers.js
+++ GraphQL/platzi-dir/lib/resolvers.js
@@ -1,37 +1,9 @@
 'use strict';
 
-const connectDB = require('./db');
-const { ObjectID } = require('mongodb');
+const queries = require('./queries');
+const mutations = require('./mutations');
 
 module.exports = {
-  Query: {
-    getCourses: async () => {
-      let db;
-      let courses = [];
-
-      try {
-        db = await connectDB();
-        courses = await db.collection('courses').find().toArray();
-      } catch (error) {
-        console.error(error);
-      }
-
-      return courses;
-    },
-    getCourse: async (root, args) => {
-      let db;
-      let course;
-
-      try {
-        db = await connectDB();
-        course = await db
-          .collection('courses')
-          .findOne({ _id: ObjectID(args.id) });
-      } catch (error) {
-        console.error(error);
-      }
-
-      return course;
-    },
-  },
+  Query: queries,
+  Mutation: mutations,
 };
