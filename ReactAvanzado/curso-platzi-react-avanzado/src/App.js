664a821f61c084210c59b0458ca35619761088e3
jzamora5 <jzamora_5@yahoo.com>
2021-06-22 10:35:48 -0500

    Add List of Categories and Global Styles

--- ReactAvanzado/curso-platzi-react-avanzado/src/App.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
-export const App = () => {
-  return <Category />
-}
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
+)
