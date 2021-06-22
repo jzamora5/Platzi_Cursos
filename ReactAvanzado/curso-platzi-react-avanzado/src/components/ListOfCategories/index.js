f08c92549ced42918430851ba217f13bc3760300
jzamora5 <jzamora_5@yahoo.com>
2021-06-22 12:14:34 -0500

    Start using React Hooks

--- ReactAvanzado/curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+// import { categories as mockCategories } from '../../../api/db.json'
+
+const API_URL_CATEGORIES = 'https://petgram-server-jhoan-8e4aw2sxk-jzamora5.vercel.app/categories'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch(API_URL_CATEGORIES).then(res => res.json()).then(response => {
+      setCategories(response)
+    })
+  }, [])
+
   return (
     <List>
       {
