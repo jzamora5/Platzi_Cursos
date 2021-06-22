95bc8667eff443831ccc3ebed2f4a4680357c019
jzamora5 <jzamora_5@yahoo.com>
2021-06-22 12:03:15 -0500

    Add Animations

--- ReactAvanzado/curso-platzi-react-avanzado/src/App.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Logo } from './Logo'
+import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => (
   <div>
