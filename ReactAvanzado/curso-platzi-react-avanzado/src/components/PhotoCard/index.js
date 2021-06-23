a89586d7a0ae9576a186702b6b8f68bd1dec3070
jzamora5 <jzamora_5@yahoo.com>
2021-06-23 13:42:15 -0500

    Polyfill and dynamic import

--- ReactAvanzado/curso-platzi-react-avanzado/src/components/PhotoCard/index.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/components/PhotoCard/index.js
@@ -9,16 +9,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        console.log('si')
-        setShow(true)
-        observer.disconnect()
-      }
-    })
+    Promise.resolve(typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
 
-    observer.observe(element.current)
+      observer.observe(element.current)
+    })
   }, [element])
 
   return (
