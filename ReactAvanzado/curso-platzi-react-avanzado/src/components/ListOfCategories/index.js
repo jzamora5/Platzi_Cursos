92fe8e4ae9f8e390ad02d55b65b93fa382fe9ead
jzamora5 <jzamora_5@yahoo.com>
2021-06-23 11:02:37 -0500

    Scroll Event Listener with UseEffect

--- ReactAvanzado/curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
@@ -8,6 +8,7 @@ const API_URL_CATEGORIES = 'https://petgram-server-jhoan-8e4aw2sxk-jzamora5.verc
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch(API_URL_CATEGORIES).then(res => res.json()).then(response => {
@@ -15,11 +16,30 @@ export const ListOfCategories = () => {
     })
   }, [])
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-            categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
+        categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
