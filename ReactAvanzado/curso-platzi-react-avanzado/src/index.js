acb8ca3d80da322750b6eb61e66c85e1ac620574
jzamora5 <jzamora_5@yahoo.com>
2021-06-29 18:33:46 -0500

    Using React Apollo

--- ReactAvanzado/curso-platzi-react-avanzado/src/index.js
+++ ReactAvanzado/curso-platzi-react-avanzado/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-jhoan-8e4aw2sxk-jzamora5.vercel.app/graphql'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('app'))
