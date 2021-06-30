09c4489ab130d7dfdeb1e33bccd4a4505324eb2f
jzamora5 <jzamora_5@yahoo.com>
2021-06-29 19:57:12 -0500

    Create and delete user movies

--- Autenticacion_con_PassportJS/ssr-server/index.js
+++ Autenticacion_con_PassportJS/ssr-server/index.js
@@ -28,7 +28,7 @@ app.post('/auth/sign-in', async function (req, res, next) {
         next(boom.unauthorized());
       }
 
-      const { token, ...user } = data;
+      const { token, user } = data;
 
       req.login(data, { session: false }, async function (error) {
         if (error) {
@@ -63,9 +63,48 @@ app.post('/auth/sign-up', async function (req, res, next) {
 
 app.get('/movies', async function (req, res, next) {});
 
-app.post('/user-movies', async function (req, res, next) {});
+app.post('/user-movies', async function (req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
 
-app.delete('/user-movies/:userMovieId', async function (req, res, next) {});
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'post',
+      data: userMovie,
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/user-movies/:userMovieId', async function (req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'delete',
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
