f49a1f2fae5e706652f6f28cc536ff67fb0be393
jzamora5 <jzamora_5@yahoo.com>
2021-07-14 20:19:13 -0500

    Building a PWA

--- ReactAvanzado/curso-platzi-react-avanzado/webpack.config.js
+++ ReactAvanzado/curso-platzi-react-avanzado/webpack.config.js
@@ -1,4 +1,9 @@
+/* eslint-disable prefer-regex-literals */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebPackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -14,6 +19,46 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebPackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
+      name: 'IntaPlatzi',
+      description: 'Tu app preferida para encontrar esas mascotas que tanto te encantan',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      background_color: '#456BD9',
+      theme_color: '#456BD9',
+      prefer_related_applications: true,
+      icons: [
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          size: '1024x1024',
+          sizes: [96, 128, 144, 192, 256, 384, 512],
+          purpose: 'maskable any',
+          destination: path.join('Icons'),
+          ios: true
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-tony-to-var-anthonytorresvargas.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
