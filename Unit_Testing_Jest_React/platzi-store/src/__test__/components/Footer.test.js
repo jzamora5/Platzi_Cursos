7d1a78cd24b4216938d1e5808aa95be699df2712
Jhoan Zamora <jzamora_5@yahoo.com>
2021-07-19 08:59:08 -0500

    Add base project with basic tests and mocks

---
+++ Unit_Testing_Jest_React/platzi-store/src/__test__/components/Footer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+  test('Render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
