494c3edfb0ed8a1a4997d3ed141347454a276636
Jhoan Zamora <jzamora_5@yahoo.com>
2021-07-22 08:28:45 -0500

    Testing with snapshots and testing actions

--- Unit_Testing_Jest_React/platzi-store/src/__test__/components/Header.test.js
+++ Unit_Testing_Jest_React/platzi-store/src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -20,5 +21,16 @@ describe('<Header />', () => {
       </ProviderMock>
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header SnapShot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
