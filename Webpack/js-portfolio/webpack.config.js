c51f6a275c34a6ed3855a699f90e1e687812b789
jzamora5 <jzamora_5@yahoo.com>
2021-08-19 20:00:03 -0500

    Add HTML and CSS Loaders

--- Webpack/js-portfolio/webpack.config.js
+++ Webpack/js-portfolio/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,6 +20,18 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
