1d5288901e1b5fd146e83bec0de3b6f6a0646bb6
jzamora5 <jzamora_5@yahoo.com>
2021-09-14 18:24:22 -0500

    Dev and Prod modes configs

--- Webpack/js-portfolio/webpack.config.dev.js
+++ Webpack/js-portfolio/webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -14,6 +11,7 @@ module.exports = {
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
+  mode: 'development',
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -74,10 +72,5 @@ module.exports = {
       ],
     }),
     new Dotenv(),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
 };
