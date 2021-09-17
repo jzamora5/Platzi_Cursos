714198e23f3ec2321405b03aaa36586fe82e3db0
jzamora5 <jzamora_5@yahoo.com>
2021-09-16 19:56:14 -0500

    Dev server and bundle analyzer

--- Webpack/js-portfolio/webpack.config.dev.js
+++ Webpack/js-portfolio/webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,7 +13,7 @@ module.exports = {
     assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
   mode: 'development',
-  watch: true,
+  // watch: true,
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -73,5 +74,13 @@ module.exports = {
       ],
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true,
+  },
 };
