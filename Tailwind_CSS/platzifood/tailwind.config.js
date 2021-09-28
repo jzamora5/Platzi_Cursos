5235bc154f1a6deb654e6feaf5ed11397a0a1ad7
Jhoan Zamora <jhoan.zamora@wizeline.com>
2021-09-28 08:38:07 -0500

    Footer and banner

--- Tailwind_CSS/platzifood/tailwind.config.js
+++ Tailwind_CSS/platzifood/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         primary: "#f3c614",
         secondary: "#353535",
       },
+      height: {
+        80: "20rem",
+        100: "25rem",
+      },
     },
   },
   variants: {
