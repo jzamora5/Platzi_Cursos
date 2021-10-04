e78d09900b84c6eda1548d3fe92bfaa9037393d6
Jhoan Zamora <jhoan.zamora@wizeline.com>
2021-10-04 09:08:55 -0500

    Add Purge CSS and CSS Nano

--- Tailwind_CSS/platzifood/postcss.config.js
+++ Tailwind_CSS/platzifood/postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss({
+      content: ["./**/*.html"],
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
 };
