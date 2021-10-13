5e61300bb3c8ec52ff09a975e0f8b2cffeabc768
Jhoan Zamora <jhoan.zamora@wizeline.com>
2021-10-13 08:26:30 -0500

    HOCs

--- Patrones_Render_Composicion_React/curso-react-patrones-render/src/index.js
+++ Patrones_Render_Composicion_React/curso-react-patrones-render/src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from './App/index.js';
+import "./index.css";
+
+function App(props) {
+  return (
+    <h1>
+      ¡{props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
+
+function withSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = withSaludo("Hi")(App);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  // <App saludo="Buenas" nombre="Nath" />,
+  <AppWithSaludo nombre="Nath" />,
+  document.getElementById("root")
 );
