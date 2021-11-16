7eedfcf0931780c72d6904ff927e076c5799ba46
Jhoan Zamora <jhoan.zamora@wizeline.com>
2021-11-16 08:47:21 -0500

    Interfaces

---
+++ Fundamentos_de_TypeScript/typescript/src/interfaces.ts
@@ -0,0 +1,83 @@
+export {};
+// Funcion para mostrar una Fotografia
+enum PhotoOrientation {
+  Landscape, // 0
+  Portrait, // 1
+  Square, // 2
+  Panorama, // 3
+}
+
+interface Picture {
+  title: string;
+  date: string;
+  orientation: PhotoOrientation;
+}
+
+function showPicture(picture: Picture) {
+  console.log(
+    `[title: ${picture.title}, 
+      date: ${picture.date}, 
+      orientation: ${picture.orientation}]`
+  );
+}
+
+let myPic = {
+  title: "Test Title",
+  date: "2020-03",
+  orientation: PhotoOrientation.Landscape,
+};
+
+showPicture(myPic);
+showPicture({
+  title: "Test Title",
+  date: "2020-03",
+  orientation: PhotoOrientation.Portrait,
+  // extra: "test", // Error
+});
+
+interface PictureConfig {
+  title?: string;
+  date?: string;
+  orientation?: PhotoOrientation;
+}
+
+function generatePicture(config: PictureConfig) {
+  const pic = { title: "Default", date: "2020-03" };
+  if (config.title) {
+    pic.title = config.title;
+  }
+
+  if (config.date) {
+    pic.date = config.date;
+  }
+
+  return pic;
+}
+
+let picture = generatePicture({});
+console.log("picture", picture);
+
+picture = generatePicture({ title: "Travel Pic" });
+console.log("picture", picture);
+
+picture = generatePicture({ title: "Travel Pic", date: "2021-05" });
+console.log("picture", picture);
+
+// Interfaz: Usuario
+
+interface User {
+  readonly id: number; // solo lectura
+  username: string;
+  isPro: boolean;
+}
+
+let user: User;
+user = {
+  id: 10,
+  username: "luixaviles",
+  isPro: true,
+};
+console.log(`user`, user);
+user.username = "paparazzi";
+// user.id = 20; // Error para el compilador
+console.log(`user`, user);
