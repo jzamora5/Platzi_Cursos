63e42c3066a7749af14dcab13965cb172cfcc387
jzamora5 <jzamora_5@yahoo.com>
2021-12-10 20:15:11 -0500

    Object Define Property, Seal, Freeze

--- JavaScript_OOP/OOP_Intermedio/static/metodos_estaticos_proto_object.js
+++ JavaScript_OOP/OOP_Intermedio/static/metodos_estaticos_proto_object.js
@@ -7,14 +7,44 @@ const juan = {
   },
 };
 
-console.log(Object.keys(juan));
-console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan));
-console.log(Object.getOwnPropertyDescriptors(juan));
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
 
-Object.defineProperty(juan, "pruebaNASA", {
-  value: "extraterrestres",
-  enumerable: true,
+Object.defineProperty(juan, "navigator", {
+  value: "Chrome",
+  enumerable: false,
   writable: true,
   configurable: true,
 });
+
+Object.defineProperty(juan, "editor", {
+  value: "VSCode",
+  enumerable: true,
+  writable: false,
+  configurable: true,
+});
+
+Object.defineProperty(juan, "terminal", {
+  value: "WSL",
+  enumerable: true,
+  writable: true,
+  configurable: false,
+});
+
+Object.defineProperty(juan, "pruebaNASA", {
+  value: "extraterrestres",
+  enumerable: false,
+  writable: false,
+  configurable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+Object.seal(juan);
+
+console.log("sealed", Object.getOwnPropertyDescriptors(juan));
+
+Object.freeze(juan);
+
+console.log("frozen", Object.getOwnPropertyDescriptors(juan));
