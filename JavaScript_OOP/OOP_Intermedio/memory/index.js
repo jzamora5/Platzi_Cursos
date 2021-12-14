b4f478ba74bd44ca12874de9cc1dbe5390b7e38c
jzamora5 <jzamora_5@yahoo.com>
2021-12-13 20:41:22 -0500

    Shallow Copy

---
+++ JavaScript_OOP/OOP_Intermedio/memory/index.js
@@ -0,0 +1,17 @@
+const obj1 = {
+  a: "a",
+  b: "b",
+  c: {
+    d: "d",
+    e: "e",
+  },
+};
+
+const obj2 = {};
+
+for (prop in obj1) {
+  obj2[prop] = obj1[prop];
+}
+
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
