09ce52820faf99057dd2482cdac256a6965f8248
jzamora5 <jzamora_5@yahoo.com>
2022-01-20 19:55:34 -0500

    Deep copy with recursion

--- JavaScript_OOP/OOP_Intermedio/memory/index.js
+++ JavaScript_OOP/OOP_Intermedio/memory/index.js
@@ -54,3 +54,41 @@ function recursiva(numbersArray) {
 }
 
 recursiva(numeritos);
+
+function isObject(subject) {
+  return typeof subject === "object";
+}
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+  let copySubject;
+
+  const subjectIsArray = isArray(subject);
+  const subjectIsObject = isObject(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
