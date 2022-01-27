e68296216b499fee6a03acbe6ed36b7d3bc8442f
Jhoan Zamora <jhoan.zamora@wizeline.com>
2022-01-27 08:11:26 -0500

    Statis methods in prototypes

--- JavaScript_OOP/OOP_Intermedio/abstraccion_encapsulamiento/index.js
+++ JavaScript_OOP/OOP_Intermedio/abstraccion_encapsulamiento/index.js
@@ -1,6 +1,7 @@
 function isObject(subject) {
   return typeof subject === "object";
 }
+
 function isArray(subject) {
   return Array.isArray(subject);
 }
@@ -35,6 +36,84 @@ function deepCopy(subject) {
 
   return copySubject;
 }
+
+// class SuperObject {
+//   static isObject(subject) {
+//     return typeof subject === "object";
+//   }
+
+//   static isArray(subject) {
+//     return Array.isArray(subject);
+//   }
+
+//   static deepCopy(subject) {
+//     let copySubject;
+
+//     const subjectIsArray = isArray(subject);
+//     const subjectIsObject = isObject(subject);
+
+//     if (subjectIsArray) {
+//       copySubject = [];
+//     } else if (subjectIsObject) {
+//       copySubject = {};
+//     } else {
+//       return subject;
+//     }
+
+//     for (key in subject) {
+//       const keyIsObject = isObject(subject[key]);
+
+//       if (keyIsObject) {
+//         copySubject[key] = deepCopy(subject[key]);
+//       } else {
+//         if (subjectIsArray) {
+//           copySubject.push(subject[key]);
+//         } else {
+//           copySubject[key] = subject[key];
+//         }
+//       }
+//     }
+
+//     return copySubject;
+//   }
+// }
+
+function SuperObject() {}
+
+SuperObject.isObject = function isObject(subject) {
+  return typeof subject === "object";
+};
+
+SuperObject.deepCopy = function deepCopy(subject) {
+  let copySubject;
+
+  const subjectIsArray = isArray(subject);
+  const subjectIsObject = isObject(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+};
 
 // Abstracción y Encapsulamiento con DeepCopy
 
