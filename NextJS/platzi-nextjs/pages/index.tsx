27e8906f9e5b6452399a87f1ab26be1daa556a52
Jhoan Zamora <jhoan.zamora@wizeline.com>
2022-02-07 08:30:37 -0500

    getServerSideProps

--- NextJS/platzi-nextjs/pages/index.tsx
+++ NextJS/platzi-nextjs/pages/index.tsx
@@ -3,17 +3,19 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
+  const [, setProductList] = useState<TProduct[]>([])
 
   return (
     <Layout>
