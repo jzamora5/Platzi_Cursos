60d723929aebfa736891f85d6d5becaf34525f48
Jhoan Zamora <jhoan.zamora@wizeline.com>
2022-02-07 08:53:50 -0500

    SSG with dynamic paths

--- NextJS/platzi-nextjs/pages/product/[id].tsx
+++ NextJS/platzi-nextjs/pages/product/[id].tsx
@@ -1,23 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+// Hay que indicar cuales son todas las paginas para SSG
 
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+export const getStaticPaths = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+
+  const paths = productList.map(({ id }) => {
+    return {
+      params: {
+        id,
+      },
     }
-  }, [query.id])
+  })
+
+  return {
+    paths,
+    // incremental static generation
+    //  404 for everything else
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // pages-only
+  const id = params?.id as string
+
+  const response = await fetch(`http://localhost:3000/api/avo/${id}`)
+  const product = await response.json()
+
+  return {
+    props: {
+      product,
+    },
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct }) => {
+  // const { query } = useRouter()
+  // const [, setProduct] = useState<TProduct | null>(null)
 
   return (
     <Layout>
