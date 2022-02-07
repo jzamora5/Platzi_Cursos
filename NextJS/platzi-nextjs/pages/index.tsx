863fb59ede8237bcd6adf331a5274701b3ecbfed
Jhoan Zamora <jhoan.zamora@wizeline.com>
2022-02-07 08:38:39 -0500

    getStaticProps

--- NextJS/platzi-nextjs/pages/index.tsx
+++ NextJS/platzi-nextjs/pages/index.tsx
@@ -3,7 +3,19 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+//   const response = await fetch('http://localhost:3000/api/avo')
+//   const { data: productList }: TAPIAvoResponse = await response.json()
+
+//   return {
+//     props: {
+//       productList,
+//     },
+//   }
+// }
+
+export const getStaticProps = async () => {
+  // pages-only
   const response = await fetch('http://localhost:3000/api/avo')
   const { data: productList }: TAPIAvoResponse = await response.json()
 
