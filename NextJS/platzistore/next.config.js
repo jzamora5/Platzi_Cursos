a04dad159a848a484e35923642247fa350e49317
jzamora5 <jzamora_5@yahoo.com>
2022-02-14 08:39:38 -0500

    Configs for project

--- NextJS/platzistore/next.config.js
+++ NextJS/platzistore/next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue'
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/hello',
+        permantent: true
+      }
+    ]
+  }
 };
