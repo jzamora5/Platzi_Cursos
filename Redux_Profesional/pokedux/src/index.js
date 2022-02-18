88159a99eec905f1780f8331101f2a00445c5cec
jzamora5 <jzamora_5@yahoo.com>
2022-02-18 08:35:36 -0500

    Creating your own middleware

--- Redux_Profesional/pokedux/src/index.js
+++ Redux_Profesional/pokedux/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import pokemonReducer from "./reducers/pokemonReducer";
+import { logActions } from "./middlewares";
 
-const store = createStore(
-  pokemonReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logActions)
 );
+
+const store = createStore(pokemonReducer, composedEnhancers);
 
 ReactDOM.render(
   <Provider store={store}>
