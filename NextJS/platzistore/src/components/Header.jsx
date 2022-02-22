e6e0beb5540be4be1ff1d1640c424d98a38c25b7
jzamora5 <jzamora_5@yahoo.com>
2022-02-22 08:12:01 -0500

    Fix image bug cause by default next styles in img

--- NextJS/platzistore/src/components/Header.jsx
+++ NextJS/platzistore/src/components/Header.jsx
@@ -10,61 +10,53 @@ import shoppingCart from '@icons/icon_shopping_cart.svg';
 import styles from '@styles/Header.module.scss';
 
 const Header = () => {
-	const { state, toggleOrder, toggleMenu } = useContext(AppContext);
+  const { state, toggleOrder, toggleMenu } = useContext(AppContext);
 
-	return (
-		<>
-			<nav className={styles.Nav}>
-				<img src={menu.src} alt="menu" className={styles.menu} />
-				<div className={styles['navbar-left']}>
-					<Link href="/" passHref>
-						<Image src={logo} alt="logo" className={styles['nav-logo']} />
-					</Link>
-					<ul>
-						<li>
-							<Link href="/">Linkll</Link>
-						</li>
-						<li>
-							<Link href="/">Clothes</Link>
-						</li>
-						<li>
-							<Link href="/">Electronics</Link>
-						</li>
-						<li>
-							<Link href="/">Furnitures</Link>
-						</li>
-						<li>
-							<Link href="/">Toys</Link>
-						</li>
-						<li>
-							<Link href="/">Others</Link>
-						</li>
-					</ul>
-				</div>
-				<div className={styles['navbar-right']}>
-					<ul>
-						<li
-							className={styles['more-clickable-area'], styles['navbar-email'], styles.pointer}
-							onClick={() => toggleMenu()}
-							aria-hidden="true"
-						>
-							platzi@example.com
-						</li>
-						<li
-							className={styles['navbar-shopping-cart']}
-							onClick={() => toggleOrder()}
-							aria-hidden="true"
-						>
-							<Image className={styles['more-clickable-area'], styles.pointer} src={shoppingCart} alt="shopping cart" />
-							{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
-						</li>
-					</ul>
-				</div>
-				{state.menuIsOpen && <Menu />}
-			</nav>
-			{state.orderIsOpen && <MyOrder />}
-		</>
-	);
+  return (
+    <>
+      <nav className={styles.Nav}>
+        <img src={menu.src} alt="menu" className={styles.menu} />
+        <div className={styles['navbar-left']}>
+          <Link href="/" passHref>
+            <Image src={logo} alt="logo" className={styles['nav-logo']} />
+          </Link>
+          <ul>
+            <li>
+              <Link href="/">Linkll</Link>
+            </li>
+            <li>
+              <Link href="/">Clothes</Link>
+            </li>
+            <li>
+              <Link href="/">Electronics</Link>
+            </li>
+            <li>
+              <Link href="/">Furnitures</Link>
+            </li>
+            <li>
+              <Link href="/">Toys</Link>
+            </li>
+            <li>
+              <Link href="/">Others</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles['navbar-right']}>
+          <ul>
+            <li className={(styles['more-clickable-area'], styles['navbar-email'], styles.pointer)} onClick={() => toggleMenu()} aria-hidden="true">
+              platzi@example.com
+            </li>
+            <li className={styles['navbar-shopping-cart']} onClick={() => toggleOrder()} aria-hidden="true">
+              <Image className={(styles['more-clickable-area'], styles.pointer)} src={shoppingCart} alt="shopping cart" />
+              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+            </li>
+          </ul>
+        </div>
+        {state.menuIsOpen && <Menu />}
+      </nav>
+      {state.orderIsOpen && <MyOrder />}
+    </>
+  );
 };
 
 export default Header;
