051ac9c6fac1362c93c0dcd4e5f20380996fe67f
jzamora5 <jzamora_5@yahoo.com>
2022-03-04 09:18:01 -0500

    Redux Toolkit

--- Redux_Profesional/pokedux/src/index.js
+++ Redux_Profesional/pokedux/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import App from './containers/App';
-import { logAction, reportError } from './middlewares';
-import rootReducer from './reducers/rootReducer';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./containers/App";
+// import { logAction, reportError } from './middlewares';
+import rootReducer from "./reducers/rootReducer";
+import "./index.css";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const composedEnhancers = composeEnhancers(
-  applyMiddleware(thunk, logAction, reportError)
+  // applyMiddleware(thunk, logAction, reportError)
+  applyMiddleware(thunk)
 );
 
 const store = createStore(rootReducer, composedEnhancers);
@@ -20,5 +21,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- Redux_Profesional/pokedux/src/middlewares/index.js
+++ Redux_Profesional/pokedux/src/middlewares/index.js
@@ -1,16 +1,16 @@
-import { SET_ERROR } from '../actions/types';
+// import { SET_ERROR } from "../actions/types";
 
-export const logAction = (store) => (next) => (actionInfo) => {
-  console.log('dispatching', actionInfo);
-  next(actionInfo);
-};
+// export const logAction = (store) => (next) => (actionInfo) => {
+//   console.log('dispatching', actionInfo);
+//   next(actionInfo);
+// };
 
-export const reportError = (store) => (next) => (actionInfo) => {
-  const { action } = actionInfo;
+// export const reportError = (store) => (next) => (actionInfo) => {
+//   const { action } = actionInfo;
 
-  if (action?.type === SET_ERROR) {
-    console.error(action.payload);
-  }
+//   if (action?.type === SET_ERROR) {
+//     console.error(action.payload);
+//   }
 
-  next(actionInfo);
-};
+//   next(actionInfo);
+// };
