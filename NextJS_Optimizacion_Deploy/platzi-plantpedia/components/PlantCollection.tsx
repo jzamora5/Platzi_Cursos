4aca4067774c382a1c2874363675c4fed00a60ab
jzamora5 <jzamora_5@yahoo.com>
2022-07-22 09:18:19 -0500

    Shallow Routing

--- NextJS_Optimizacion_Deploy/platzi-plantpedia/components/PlantCollection.tsx
+++ NextJS_Optimizacion_Deploy/platzi-plantpedia/components/PlantCollection.tsx
@@ -1,10 +1,10 @@
-import NextImage, { ImageLoaderProps } from 'next/image'
 import Link from 'next/link'
 import { Grid, GridProps } from '@ui/Grid'
 import { Typography } from '@ui/Typography'
 import { Button } from '@ui/Button'
 
 import { Excerpt } from '@components/Excerpt'
+import { Image } from '@components/Image'
 
 type PlantCollectionProps = {
   plants: Plant[]
@@ -50,61 +50,16 @@ export function PlantEntry({ plant, variant = 'square' }: PlantEntryType) {
   )
 }
 
-const aspectRatioToRatio = {
-  '1:1': 1,
-  '4:3': 3 / 4,
-  '16:9': 9 / 19,
-}
-
-function calcAspectRatio(
-  aspectRatio: '1:1' | '4:3' | '16:9',
-  width: number
-): number {
-  const ratio = aspectRatioToRatio[aspectRatio]
-
-  return Math.floor(width * ratio)
-}
-
-type ImageProps = {
-  layout: 'responsive' | 'fixed' | 'intrinsic' | undefined
-  src: string
-  width: number
-  height?: never
-  aspectRatio: '1:1' | '4:3' | '16:9'
-  fit?: 'pad' | 'fill' | 'crop' | 'scale'
-}
-
-function Image({ layout, src, width, aspectRatio, fit = 'scale' }: ImageProps) {
-  const height = calcAspectRatio(aspectRatio, width)
-
-  const loader = (args: ImageLoaderProps): string => {
-    const loaderHeight = calcAspectRatio(aspectRatio, args.width)
-
-    return `${args.src}?w=${width}&h=${loaderHeight}&fit=${fit}`
-  }
-
-  return (
-    <NextImage
-      layout={layout}
-      src={src}
-      width={width}
-      height={height}
-      loader={loader}
-    />
-  )
-}
-
 export function PlantEntrySquare({ image, plantName, slug }: Plant) {
   return (
     <Link href={`/entry/${slug}`}>
       <a title={`Go to ${plantName}`}>
         <div className="opacity-95 hover:opacity-100">
           <Image
-            layout="responsive"
-            aspectRatio="4:3"
             src={image.url}
+            layout="intrinsic"
             width={460}
-            fit="scale"
+            aspectRatio="4:3"
           />
           <div className="p-4">
             <Typography variant="h4" className="break-words">
@@ -129,7 +84,14 @@ export function PlantEntryInline({
         <div
           className={`opacity-95 hover:opacity-100 flex items-end ${className}`}
         >
-          <img src={image.url} width={84} className="flex-none" />
+          <Image
+            src={image.url}
+            layout="fixed"
+            width={84}
+            aspectRatio="1:1"
+            fit="fill"
+            className="flex-none"
+          />
           <div className="pl-2 flex-auto">
             <Typography variant="h6" className="break-words">
               {plantName}
@@ -151,7 +113,12 @@ export function PlantEntryVertical({
     <div className="opacity-95 hover:opacity-100">
       <Link href={`/entry/${slug}`}>
         <a title={`Go to ${plantName}`}>
-          <img src={image.url} width={624} />
+          <Image
+            src={image.url}
+            width={624}
+            layout="intrinsic"
+            aspectRatio="9:12"
+          />
           <Typography variant="h2" className="break-words pt-4 px-4">
             {plantName}
           </Typography>
