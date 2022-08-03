c8ffc75b17f5b80405a24449ef5283dfd71fd2e0
jzamora5 <jzamora_5@yahoo.com>
2022-08-02 21:31:15 -0500

    Base Project

---
+++ NodeJS_Auth_MicroSer_Redis/api/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const config = require("../config.js");
+const user = require("../components/user/network.js");
+
+const app = express();
+
+// ROUTES
+
+app.use("/api/user", user);
+
+app.listen(config.api.port, () => {
+  console.log(`API escuchando http://localhost:${config.api.port}`);
+});
