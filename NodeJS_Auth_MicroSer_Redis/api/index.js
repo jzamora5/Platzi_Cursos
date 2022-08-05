49f88069963db6591cb9effe6a13a54f5f25a0a3
jzamora5 <jzamora_5@yahoo.com>
2022-08-04 20:39:54 -0500

    Add Swagger

--- NodeJS_Auth_MicroSer_Redis/api/index.js
+++ NodeJS_Auth_MicroSer_Redis/api/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 
+const swaggerUi = require("swagger-ui-express");
+
 const config = require("../config.js");
 const user = require("./components/user/network.js");
 
 const app = express();
 
-// ROUTES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const swaggerDoc = require("./swagger.json");
+
+// ROUTES
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log(`API escuchando http://localhost:${config.api.port}`);
