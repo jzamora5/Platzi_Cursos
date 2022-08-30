e5184f42ec0a86ab9480a32a661c33ae5594637c
jzamora5 <jzamora_5@yahoo.com>
2022-08-29 20:53:03 -0500

    Connecting Redis

--- NodeJS_Auth_MicroSer_Redis/config.js
+++ NodeJS_Auth_MicroSer_Redis/config.js
@@ -18,4 +18,14 @@ module.exports = {
     host: process.env.MYSQL_SRT_HOST || "localhost",
     port: process.env.MYSQL_SRV_PORT || 3001,
   },
+  cacheService: {
+    host: process.env.CACHE_SRT_HOST || "localhost",
+    port: process.env.CACHE_SRV_PORT || 3003,
+  },
+  redis: {
+    dbUser: "default",
+    dbPass: "HHwaz1OcoYtv6aN0AIuiGnxzsaP3TxwB",
+    dbHost: "redis-19277.c8.us-east-1-4.ec2.cloud.redislabs.com",
+    dbPort: "19277",
+  },
 };
