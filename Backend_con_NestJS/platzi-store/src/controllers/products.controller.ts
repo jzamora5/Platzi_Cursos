3513f4c49083ff6851697dad58b0c30a7881299c
jzamora5 <jzamora_5@yahoo.com>
2022-10-11 20:19:52 -0500

    Put and Delete

--- Backend_con_NestJS/platzi-store/src/controllers/products.controller.ts
+++ Backend_con_NestJS/platzi-store/src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -35,6 +44,23 @@ export class ProductsController {
     return {
       message: 'accion de crear',
       payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      message: 'accion de editar',
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: 'accion de borrar',
+      id,
     };
   }
 }
