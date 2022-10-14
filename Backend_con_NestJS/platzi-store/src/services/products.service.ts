5230bf02a4e9303db45df9a32d1921502a9cbbff
jzamora5 <jzamora_5@yahoo.com>
2022-10-13 19:57:31 -0500

    Service

---
+++ Backend_con_NestJS/platzi-store/src/services/products.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId: 1;
+
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla bla',
+      price: 122,
+      stock: 1,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId += 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+
+    this.products.push(newProduct);
+
+    return newProduct;
+  }
+}
