bf4433228331852e9762aefffc3138396e70565f
jzamora5 <jzamora_5@yahoo.com>
2022-11-08 18:22:50 -0500

    Global Module

--- NestJS_Programacion_Modular/nestjs-modular/src/app.module.ts
+++ NestJS_Programacion_Modular/nestjs-modular/src/app.module.ts
@@ -4,19 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
-
-const API_KEY = '123456789';
-const API_KEY_PROD = 'PROD21212SA';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
