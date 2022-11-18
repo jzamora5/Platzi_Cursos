4dfa19d62ad0164c2f556ef0f09cff21d2af8d4c
jzamora5 <jzamora_5@yahoo.com>
2022-11-17 19:42:45 -0500

    Schema Validation with Joi

--- NestJS_Programacion_Modular/nestjs-modular/src/app.module.ts
+++ NestJS_Programacion_Modular/nestjs-modular/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -16,6 +18,11 @@ import config from './config';
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
