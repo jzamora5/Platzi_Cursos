4b12f93fc6c7081117b7f85d1f9a102737607f72
jzamora5 <jzamora_5@yahoo.com>
2022-12-27 09:31:14 -0500

    Fix selected country not showing up

--- react-state-machines/src/Components/Tickets.js
+++ react-state-machines/src/Components/Tickets.js
@@ -12,7 +12,7 @@ export const Tickets = ({ send, context }) => {
         Gracias por volar con book a fly 💚
       </p>
       <div className="Tickets-ticket">
-        <div className="Tickets-country">Colobia</div>
+        <div className="Tickets-country">{context.selectedCountry}</div>
         <div className="Tickets-passengers">
           <span>✈</span>
           {context.passengers.map((person, id) => {
