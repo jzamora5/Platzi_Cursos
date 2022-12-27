cea364a1bd33c7f091041754d3fcb545e3619bfd
jzamora5 <jzamora_5@yahoo.com>
2022-12-27 12:18:53 -0500

    Add PG connection through injection

--- NestJS_TypeORM/nestjs-typeorm/src/config.ts
+++ NestJS_TypeORM/nestjs-typeorm/src/config.ts
@@ -6,6 +6,13 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
+    postgres: {
+      dbName: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      port: parseInt(process.env.POSTGRES_PORT),
+      host: process.env.POSTGRES_HOST,
+    },
     apiKey: process.env.API_KEY,
   };
 });
--- NestJS_TypeORM/nestjs-typeorm/src/database/database.module.ts
+++ NestJS_TypeORM/nestjs-typeorm/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+import config from '../config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
@@ -10,7 +13,26 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+
+        const client = new Client({
+          user,
+          host,
+          database: dbName,
+          password,
+          port,
+        });
+
+        client.connect();
+
+        return client;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
