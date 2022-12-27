1e3d6efbe66427e8dfd0d0a32f080f271eb57cec
jzamora5 <jzamora_5@yahoo.com>
2022-12-27 12:50:22 -0500

    Adding TypeORM Connection

--- NestJS_TypeORM/nestjs-typeorm/src/database/database.module.ts
+++ NestJS_TypeORM/nestjs-typeorm/src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { Client } from 'pg';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from '../config';
 
 const API_KEY = '12345634';
@@ -8,6 +9,23 @@ const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -33,6 +51,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'PG'],
+  exports: ['API_KEY', 'PG', TypeOrmModule],
 })
 export class DatabaseModule {}
