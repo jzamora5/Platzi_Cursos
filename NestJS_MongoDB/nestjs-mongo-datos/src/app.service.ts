51421fca4823a4705a8a445fd91892a1a7f5b792
jzamora5 <jzamora_5@yahoo.com>
2023-01-19 18:57:57 -0500

    Using Mongo Driver in NestJS and using Env Variables

--- NestJS_MongoDB/nestjs-mongo-datos/src/app.service.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
 import config from './config';
 
 @Injectable()
@@ -7,11 +8,18 @@ export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject('MONGO') private database: Db,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+
+  getTasks() {
+    const tasksCollection = this.database.collection('tasks');
+
+    return tasksCollection.find().toArray();
   }
 }
--- NestJS_MongoDB/nestjs-mongo-datos/src/database/database.module.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/database/database.module.ts
@@ -1,7 +1,19 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { MongoClient } from 'mongodb';
+
+import config from '../config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+// async function run() {
+//   const taskCollection = database.collection('tasks');
+
+//   const tasks = await taskCollection.find().toArray();
+
+//   console.log(tasks);
+// }
 
 @Global()
 @Module({
@@ -10,7 +22,34 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'MONGO',
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const {
+          connection,
+          user,
+          password,
+          host,
+          port,
+          dbName,
+        } = configService.mongo;
+
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
+
+        const client = new MongoClient(uri, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+
+        await client.connect();
+
+        const database = client.db(dbName);
+
+        return database;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MONGO'],
 })
 export class DatabaseModule {}
