1c14d2334eef91e6137b3a7dcf279015556fdcd3
jzamora5 <jzamora_5@yahoo.com>
2023-02-08 19:06:18 -0500

    Nested and Reference Relationships

--- NestJS_MongoDB/nestjs-mongo-datos/src/products/entities/product.entity.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/products/entities/product.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
+import { Brand } from './brand.entity';
 @Schema()
 export class Product extends Document {
   @Prop({ required: true })
@@ -25,6 +26,12 @@ export class Product extends Document {
     }),
   )
   category: Record<string, any>;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: Brand.name,
+  })
+  brand: Brand | Types.ObjectId;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
