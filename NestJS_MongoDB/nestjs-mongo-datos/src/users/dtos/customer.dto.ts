4d256b6e3a264ea6406ee8ed7af67bc91aec45a7
jzamora5 <jzamora_5@yahoo.com>
2023-02-09 18:56:45 -0500

    1 a Muchos Embebido

--- NestJS_MongoDB/nestjs-mongo-datos/src/users/dtos/customer.dto.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/users/dtos/customer.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { IsString, IsNotEmpty, IsPhoneNumber, IsArray } from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
 
 export class CreateCustomerDto {
@@ -13,6 +13,10 @@ export class CreateCustomerDto {
   @IsPhoneNumber()
   @IsNotEmpty()
   readonly phone: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  readonly skills: any;
 }
 
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
