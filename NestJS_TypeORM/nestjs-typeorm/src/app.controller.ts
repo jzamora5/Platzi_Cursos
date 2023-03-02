c60026bfdd1860529a561c53c88f107238d5ce17
jzamora5 <jzamora_5@yahoo.com>
2023-03-01 19:01:12 -0500

    Guardians and decorators

--- NestJS_TypeORM/nestjs-typeorm/src/app.controller.ts
+++ NestJS_TypeORM/nestjs-typeorm/src/app.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('nuevo')
+  @Public()
   newEndpoint() {
     return 'yo soy nuevo';
   }
