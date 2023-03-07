d828581aeba5c6169ac8bf909b53781ee457aeb6
jzamora5 <jzamora_5@yahoo.com>
2023-03-06 19:09:30 -0500

    Add authentication to routes

--- NestJS_MongoDB/nestjs-mongo-datos/src/app.controller.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/app.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
+import { Public } from './auth/decorators/public.decorator';
+
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
 import { AppService } from './app.service';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('nuevo')
+  @Public()
   newEndpoint() {
     return 'yo soy nuevo';
   }
--- NestJS_MongoDB/nestjs-mongo-datos/src/app.module.ts
+++ NestJS_MongoDB/nestjs-mongo-datos/src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
+import { AuthModule } from './auth/auth.module';
 import config from './config';
 
 @Module({
@@ -17,7 +18,7 @@ import config from './config';
       load: [config],
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.number().required(),
+        API_KEY: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
       }),
@@ -26,6 +27,7 @@ import config from './config';
     UsersModule,
     ProductsModule,
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
