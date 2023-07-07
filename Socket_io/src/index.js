a75b8ac4635e49ed691bbd735a5c78533d978c19
Jhoan Zamora <jhoan.zamora@wizeline.com>
2023-07-07 09:39:52 -0500

    Socket events

--- Socket_io/src/index.js
+++ Socket_io/src/index.js
@@ -14,7 +14,19 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("SOCKET ID", socket.id);
+  console.log("Clientes conectados: ", io.engine.clientsCount);
+  console.log("ID del socket conectado", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("El socket " + socket.id + " se ha desconectado");
+  });
+
+  socket.conn.once("upgrade", () => {
+    console.log(
+      "Hemos pasado de HTTP Long-Polling a",
+      socket.conn.transport.name
+    );
+  });
 });
 
 httpServer.listen(3000);
---
+++ Socket_io/src/views/js/index.js
@@ -0,0 +1,27 @@
+const socket = io();
+
+function checkSocketStatus() {
+  console.log("Estado del socket:", socket.connected);
+}
+
+socket.on("connect", () => {
+  console.log("El socket se ha conectado: ", socket.id);
+  checkSocketStatus();
+});
+
+socket.on("connect_error", () => {
+  console.log("No pude conectarme 😔");
+});
+
+socket.on("disconnect", () => {
+  console.log("El socket se ha desconectado: ", socket.id);
+  checkSocketStatus();
+});
+
+socket.io.on("reconnect_attempt", () => {
+  console.log("Estoy intentando reconectarme 🖥️");
+});
+
+socket.io.on("reconnect", () => {
+  console.log("¡Me he vuelto a conectar! 😄");
+});
