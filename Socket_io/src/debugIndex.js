fcc7703196561a8e1ca882407a1fe6c9547e4e9b
Jhoan Zamora <jhoan.zamora@wizeline.com>
2023-07-14 18:41:24 -0500

    Jest test scaffolding

--- Socket_io/src/debugIndex.js
+++ Socket_io/src/debugIndex.js
@@ -1,3 +1,6 @@
+// process.env.DEBUG = "*";
+process.env.DEBUG = "engine, socket.io:socket, socket.io:client";
+
 const express = require("express");
 const path = require("path");
 const { createServer } = require("http");
@@ -13,8 +16,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
+io.of("custom-namespace").on("connection", (socket) => {
+  socket.on("circle position", (position) => {
+    socket.broadcast.emit("move circle", position);
+  });
 });
 
 httpServer.listen(3000);
---
+++ Socket_io/test/socket.test.js
@@ -0,0 +1,29 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
+
+describe("Testing Socket.io", () => {
+  let io, serverSocket, clientSocket;
+
+  beforeAll((done) => {
+    const httpServer = createServer();
+
+    io = new Server(httpServer);
+
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+
+      io.on("connection", (socket) => {
+        serverSocket = socket;
+      });
+
+      clientSocket.on("connect", done);
+    });
+  });
+
+  afterAll(() => {
+    io.close();
+    clientSocket.close();
+  });
+});
