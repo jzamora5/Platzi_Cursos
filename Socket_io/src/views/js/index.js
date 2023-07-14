b7423ac860c09e3f9e44a57904d86fb03a8d5ff8
jzamora5 <jzamora_5@yahoo.com>
2023-07-13 19:03:16 -0500

    Socket IO Middlewares

--- Socket_io/src/views/js/index.js
+++ Socket_io/src/views/js/index.js
@@ -1,31 +1,12 @@
-const socket = io();
-
-const circle = document.querySelector("#circle");
-
-const drawCircle = (position) => {
-  circle.style.top = position.top;
-  circle.style.left = position.left;
-};
-
-const drag = (e) => {
-  const position = {
-    top: e.clientY + "px",
-    left: e.clientX + "px",
-  };
-
-  drawCircle(position);
-  console.log("Se envia el evento al servidor");
-  socket.volatile.emit("circle position", position);
-};
-
-document.addEventListener("mousedown", (e) => {
-  document.addEventListener("mousemove", drag);
+const socket = io({
+  auth: {
+    token: "Mr. Michi es genial",
+  },
 });
 
-document.addEventListener("mouseup", (e) => {
-  document.removeEventListener("mousemove", drag);
-});
-
-socket.on("move circle", (position) => {
-  drawCircle(position);
+// En caso de error en el middleware
+socket.on("connect_error", (err) => {
+  console.log("Error de conexión 😔");
+  console.log(err.message);
+  console.log(err.data.details);
 });
