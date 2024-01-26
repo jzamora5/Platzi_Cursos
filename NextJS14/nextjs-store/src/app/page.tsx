dc397d8722cac91a7b15c33248ca4a530e5859be
Jhoan Zamora <jhoan.zamora@wizeline.com>
2024-01-26 09:33:44 -0500

    Add base NextJS 14 project

---
+++ NextJS14/nextjs-store/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Hola Mundo</h1>
+    </main>
+  );
+}
