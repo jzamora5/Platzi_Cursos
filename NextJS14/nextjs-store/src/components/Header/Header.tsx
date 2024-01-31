87f4289e36a8ab7c6078c893d67ffef4ccc923d3
Jhoan Zamora <jhoan.zamora@wizeline.com>
2024-01-31 18:50:30 -0500

    Add Header

---
+++ NextJS14/nextjs-store/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/store">
+            <li>Store</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
