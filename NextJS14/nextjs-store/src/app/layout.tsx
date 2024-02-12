a6c63f9c3972e560aeaf204eaedd5913b4b63b90
Jhoan Zamora <jhoan.zamora@wizeline.com>
2024-02-12 09:49:42 -0500

    Fetching products

--- NextJS14/nextjs-store/src/app/layout.tsx
+++ NextJS14/nextjs-store/src/app/layout.tsx
@@ -1,10 +1,13 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import "app/sass/globals.sass";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["100", "300", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
--- NextJS14/nextjs-store/src/components/home/MainProducts/MainProducts.tsx
+++ NextJS14/nextjs-store/src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,23 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    {
+      headers: {
+        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+
+  console.log("PRODUCTS", products);
+
   return (
     <div>
       <h1>MainProducts</h1>
